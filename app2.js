b66c410cb1f7d40af832766adf4c743656d6ebd4
Biswajit-acadgild <biswajit.146@gmail.com>
2016-11-09 19:45:54 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,19 @@
+var employee ={
+    
+    name : "Biswajit Sahu",
+    age : 23,
+    salary : 30000,
+    address :{
+        
+        city : "Bengaluru",
+        state : "Karnataka",
+        pincode : 560076
+    }
+}
+
+console.log("Name :"+employee.name);
+console.log("Age :"+employee.age);
+console.log("Salary :"+employee.salary);
+console.log("City :"+employee.address.city);
+console.log("State :"+employee.address.state);
+console.log("Pincode :"+employee.address.pincode);
